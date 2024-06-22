f3fa64a44921f5165c3ee541e1254af45dbb39da
MolchanovaOlga <designer.molchanova@gmail.com>
2024-06-22 20:48:12 +0300

    Change Home Page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,11 @@
 import AdvantagesSection from "../../components/AdvantagesSection/AdvantagesSection";
 import WelcomeSection from "../../components/WelcomeSection/WelcomeSection";
-import ExampleWaterModal from "../../components/WaterModal/ExampleWaterModal";
 
 const HomePage = () => {
   return (
     <div>
       <WelcomeSection />
       <AdvantagesSection />
-      <ExampleWaterModal />
     </div>
   );
 };
