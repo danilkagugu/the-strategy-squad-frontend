88f0ee1541b389d63c9aa6e0e36d6d58e534f8fa
volodymyr1v <volodumur.verhyn@gmail.com>
2024-06-23 01:56:49 +0300

    update task

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,16 +1,11 @@
 import WaterDetailedInfo from "../../components/WaterDetailedInfo/WaterDetailedInfo";
 import WaterMainInfo from "../../components/WaterMainInfo/WaterMainInfo";
-import Container from "../../components/Container/Container";
-import style from "./TrackerPage.module.css";
+
 const TrackerPage = () => {
   return (
     <>
-      <Container>
-        <div className={style.page_box}>
-          <WaterMainInfo />
-          <WaterDetailedInfo />
-        </div>
-      </Container>
+      <WaterMainInfo />
+      <WaterDetailedInfo />
     </>
   );
 };
