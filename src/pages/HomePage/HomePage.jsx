ab64c2076dc3ac17482a380441be954adcf9b3df
MolchanovaOlga <designer.molchanova@gmail.com>
2024-06-22 11:53:06 +0300

    Add styles

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,13 @@
 import WelcomeSection from "../../components/WelcomeSection/WelcomeSection";
+import WaterForm from "../../components/WaterForm/WaterForm";
 
 const HomePage = () => {
-  return <div><WelcomeSection /></div>;
+  return (
+    <div>
+      <WelcomeSection />
+      <WaterForm />
+    </div>
+  );
 };
 
 export default HomePage;
