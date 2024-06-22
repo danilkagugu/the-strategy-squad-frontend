0996995da5e10fa439afc46c1419691be094c29b
Alla10475 <sopinskaya10475@gmail.com>
2024-06-22 14:26:51 +0300

    add AdvantagesSection

---
+++ src/components/AdvantagesSection/AdvantagesSection.jsx
@@ -0,0 +1,83 @@
+import css from "./AdvantagesSection.module.css";
+
+const AdvantagesSection = () => {
+  return (
+    <div className={css.sectionWrapper}>
+      <div className={css.customersWrapper}>
+        <div>
+          <ul className={css.imgWrapper}>
+            <li className={css.customerOne}>
+              <picture>
+                <source
+                  media="(min-width: 768px)"
+                  srcSet="src\assets\customers\desktop-tablet\customers1-tab-desc.png, src\assets\customers\desktop-tablet\customers1-tab-desc@2x.png 2x"
+                />
+                <source
+                  media="(max-width: 767px)"
+                  srcSet="src\assets\customers\mobile\customers1-mob.png, src\assets\customers\mobile\customers1-mob@2x.png 2x"
+                />
+                <img
+                  src="src\assets\customers\mobile\customers1-mob.png"
+                  alt="girl"
+                  className={css.personImg}
+                />
+              </picture>
+            </li>
+            <li className={css.customerTwo}>
+              <picture>
+                <source
+                  media="(min-width: 768px)"
+                  srcSet="src\assets\customers\desktop-tablet\customers2-tab-desc.png, src\assets\customers\desktop-tablet\customers2-tab-desc@2x.png 2x"
+                />
+                <source
+                  media="(max-width: 767px)"
+                  srcSet="src\assets\customers\mobile\customers2-mob.png, src\assets\customers\mobile\customers2-mob@2x.png 2x"
+                />
+                <img
+                  src="src\assets\customers\mobile\customers2-mob.png"
+                  alt="boy"
+                  className={css.personImg}
+                />
+              </picture>
+            </li>
+            <li className={css.customerThree}>
+              <picture>
+                <source
+                  media="(min-width: 768px)"
+                  srcSet="src\assets\customers\desktop-tablet\customers3-tab-desc.png, src\assets\customers\desktop-tablet\customers3-tab-desc@2x.png 2x"
+                />
+                <source
+                  media="(max-width: 767px)"
+                  srcSet="src\assets\customers\mobile\customers3-mob.png, src\assets\customers\mobile\customers3-mob@2x.png 2x"
+                />
+                <img
+                  src="src\assets\customers\mobile\customers3-mob.png"
+                  alt="girl"
+                  className={css.personImg}
+                />
+              </picture>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p className={css.text}>
+            Our <span className={css.textAccent}>happy</span> customers
+          </p>
+        </div>
+      </div>
+      <ul className={css.benefits}>
+        <li>
+          <p className={`${css.habit} ${css.item}`}>Habit drive</p>
+        </li>
+        <li>
+          <p className={`${css.view} ${css.item}`}>View statistics</p>
+        </li>
+        <li>
+          <p className={`${css.personal} ${css.item}`}>Personal rate setting</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default AdvantagesSection;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,10 @@
+import AdvantagesSection from "../../components/AdvantagesSection/AdvantagesSection";
 import WelcomeSection from "../../components/WelcomeSection/WelcomeSection";
 
 const HomePage = () => {
-  return <div><WelcomeSection /></div>;
+  return <div><WelcomeSection />
+  <AdvantagesSection />
+  </div>;
 };
 
 export default HomePage;
