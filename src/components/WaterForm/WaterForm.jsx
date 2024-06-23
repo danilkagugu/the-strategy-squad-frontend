24e05e7185c36113a491720c56193a035fff94f6
MolchanovaOlga <designer.molchanova@gmail.com>
2024-06-23 19:55:57 +0300

    Add functions counter state and disabled style of icons

--- src/components/WaterForm/WaterForm.jsx
+++ src/components/WaterForm/WaterForm.jsx
@@ -1,4 +1,6 @@
 import { useForm } from "react-hook-form";
+import { useState } from "react";
+import clsx from "clsx";
 
 import sprite from "../../assets/icons.svg";
 import css from "./WaterForm.module.css";
@@ -10,24 +12,49 @@ const WaterForm = () => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => console.log(data);
+  const [counter, setCounter] = useState(50);
 
-  // console.log(register);
+  const minusValue = () => {
+    if (counter <= 0) {
+      return;
+    }
+    setCounter((prev) => prev - 50);
+  };
+
+  const plusValue = () => {
+    if (counter >= 5000) {
+      return;
+    }
+    setCounter((prev) => prev + 50);
+  };
+
+  const onSubmit = (data) => console.log(data);
 
   return (
     <form className={css.form} onSubmit={handleSubmit(onSubmit)}>
       <p className={css.text}>Amount of water:</p>
       <div className={css.counterContainer}>
-        <button className={css.btn} type="button">
-          <svg className={css.btnIcon} width="40" height="40">
+        <button className={css.btn} onClick={minusValue} type="button">
+          <svg
+            className={clsx("btnIcon", counter <= 0 ? css.disable : css.enable)}
+            width="40"
+            height="40"
+          >
             <use href={`${sprite}#icon-minus`}></use>
           </svg>
         </button>
         <div className={css.valueContainer}>
-          <span>50 ml</span>
+          <span>{counter} ml</span>
         </div>
-        <button className={css.btn} type="button">
-          <svg className={css.btnIcon} width="40" height="40">
+        <button className={css.btn} onClick={plusValue} type="button">
+          <svg
+            className={clsx(
+              "btnIcon",
+              counter >= 5000 ? css.disable : css.enable
+            )}
+            width="40"
+            height="40"
+          >
             <use href={`${sprite}#icon-plus`}></use>
           </svg>
         </button>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import AdvantagesSection from "../../components/AdvantagesSection/AdvantagesSection";
 import WelcomeSection from "../../components/WelcomeSection/WelcomeSection";
+import ExampleWaterModal from "../../components/WaterModal/ExampleWaterModal";
 
 const HomePage = () => {
   return (
     <div>
       <WelcomeSection />
       <AdvantagesSection />
+      <ExampleWaterModal />
     </div>
   );
 };
