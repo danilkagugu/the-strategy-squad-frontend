d6f671f2ede8342a398cd71cdbec27f4d023cb47
Danulo <danilyanishevski2001@gmail.com>
2024-06-23 16:55:37 +0300

    correct Seatings modal

--- src/components/UserSettingsModal/UserSettingsModal.jsx
+++ src/components/UserSettingsModal/UserSettingsModal.jsx
@@ -1,13 +1,21 @@
-import UserSettingsForm from '../UserSettingsForm/UserSettingsForm';
-import css from './UserSettingsModal.module.css';
+import UserSettingsForm from "../UserSettingsForm/UserSettingsForm";
+import css from "./UserSettingsModal.module.css";
+import sprite from "../../assets/icons.svg";
 
 const UserSettingsModal = ({ onClose }) => {
-	return (
-		<div className={css.settingsModal}>
-			<h3 className={css.title}>Setting</h3>
-			<UserSettingsForm onClose={onClose} />
-		</div>
-	);
+  return (
+    <div className={css.settingsModal}>
+      <div className={css.headerSeatingsModal}>
+        <h3 className={css.title}>Setting</h3>
+        <button className={css.btnClouse} onClick={onClose}>
+          <svg className={css.iconClose}>
+            <use href={`${sprite}#icon-close`}></use>
+          </svg>
+        </button>
+      </div>
+      <UserSettingsForm />
+    </div>
+  );
 };
 
 export default UserSettingsModal;
