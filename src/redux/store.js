9a8448e294eeca1a8f05c7f13dead6995ccdaaa9
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2024-06-24 21:46:42 +0100

    some small changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,37 +1,35 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./auth/slice";
 import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-
+import { waterReducer } from "./water/slice";
 
 const authConfig = {
-    key: "auth",
-    storage,
-    whitelist: ["token"],
-
+  key: "auth",
+  storage,
+  whitelist: ["token"],
 };
 
 export const store = configureStore({
-    reducer: {
-        auth: persistReducer(authConfig, authReducer),
-    },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-            },
-        }),
+  reducer: {
+    auth: persistReducer(authConfig, authReducer),
+    water: waterReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
-
-
--- src/services/waterApi.js
+++ src/services/waterApi.js
@@ -1,13 +1,10 @@
 import { instance } from "./authApi";
 
-instance.defaults.headers.common.Authorization =
-  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2Nzg4YTJlYWQzZTBhM2VmMWQ2ZTJmNSIsImlhdCI6MTcxOTE4MjI1MywiZXhwIjoxNzE5MjY1MDUzfQ.PQn4KGaA3J19IyffnWR-2TJspNqNo5DNh-9ZEUVHwYw";
-
 export const recordsPerDay = async (query) => {
   const params = {
     day: query,
   };
-  const data = await instance.get("/water/day", { params });
+  const data = await instance.get("/api/water/day", { params });
   return data;
 };
 
@@ -15,21 +12,21 @@ export const recordsPerMonth = async (query) => {
   const params = {
     month: query,
   };
-  const data = await instance.get("/water/month", { params });
+  const data = await instance.get("/api/water/month", { params });
   return data;
 };
 
 export const createWaterRecord = async (body) => {
-  const data = await instance.post("/water", body);
+  const data = await instance.post("/api/water", body);
   return data;
 };
 
 export const removeWaterRecord = async (id) => {
-  const data = await instance.delete(`/water/${id}`);
+  const data = await instance.delete(`/api/water/${id}`);
   return data;
 };
 
 export const updateWaterRecord = async (id, body) => {
-  const data = await instance.put(`/water/${id}`, body);
+  const data = await instance.put(`/api/water/${id}`, body);
   return data;
 };
