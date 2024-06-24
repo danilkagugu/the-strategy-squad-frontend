a1e45894fdfc7a8f9881d5c2510e1b98215c5736
Олег Слюсар <eternityonly1@gmail.com>
2024-06-24 08:47:52 +0300

    Remade modal with ModalWrapper
    Functinality not added yet

--- src/components/UserBarPopover/UserBarPopover.jsx
+++ src/components/UserBarPopover/UserBarPopover.jsx
@@ -1,36 +1,35 @@
-import { useState } from "react";
-import sprite from "../../assets/icons.svg";
-import css from "./UserBarPopover.module.css";
-import UserSettingsModal from "../UserSettingsModal/UserSettingsModal";
+import { useState } from 'react';
+import sprite from '../../assets/icons.svg';
+import css from './UserBarPopover.module.css';
+// import ModalWrapper from '../ModalWrapper/ModalWrapper';
+import UserSettingsModal from '../UserSettingsModal/UserSettingsModal';
 const UserBarPopover = () => {
-  const [openModal, setOpenModal] = useState(false);
-  const handleToggleModal = () => {
-    setOpenModal((prevState) => !prevState);
-  };
+	const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <>
-      <div className={css.barPopover}>
-        <button className={css.btnBar} onClick={handleToggleModal}>
-          <svg className={css.icon}>
-            <use href={`${sprite}#icon-settings`}></use>
-          </svg>
-          <p className={css.textBarPopover}>Setting</p>
-        </button>
-        <button className={css.btnBar}>
-          <svg className={css.icon}>
-            <use href={`${sprite}#icon-log-out`}></use>
-          </svg>
-          <p className={css.textBarPopover}>Log out</p>
-        </button>
-      </div>
-      {openModal ? (
-        <div className={css.modalOverlay}>
-          <UserSettingsModal onClose={handleToggleModal} />
-        </div>
-      ) : null}
-    </>
-  );
+	const onOpenModal = () => setIsOpen(true);
+	const onCloseModal = () => setIsOpen(false);
+
+	return (
+		<>
+			{isOpen && (
+				<UserSettingsModal onCloseModal={onCloseModal} isOpen={isOpen} />
+			)}
+			<div className={css.barPopover}>
+				<button className={css.btnBar} onClick={onOpenModal}>
+					<svg className={css.icon}>
+						<use href={`${sprite}#icon-settings`}></use>
+					</svg>
+					<p className={css.textBarPopover}>Setting</p>
+				</button>
+				<button className={css.btnBar}>
+					<svg className={css.icon}>
+						<use href={`${sprite}#icon-log-out`}></use>
+					</svg>
+					<p className={css.textBarPopover}>Log out</p>
+				</button>
+			</div>
+		</>
+	);
 };
 
 export default UserBarPopover;
