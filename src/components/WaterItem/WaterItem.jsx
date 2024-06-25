463abfc137e5aa47a7cbc44f4ea3a53e7c9145ef
ValiaStebivka <valia.stebivka2016@gmail.com>
2024-06-25 16:14:26 +0300

    add WaterList & WaterItem components

---
+++ src/components/WaterItem/WaterItem.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import css from "./WaterItem.module.css";
+import sprite from "../../assets/sprite.svg";
+import { BaseModal } from "../BaseModal/BaseModal";
+import { WaterModal } from "../WaterModal/WaterModal";
+import { DeleteModal } from "../DeleteModal/DeleteModal";
+import { GlobalModal } from "../globalModal/globalModal";
+
+export function WaterItem({ item }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const openModal = () => {
+    setIsOpen(true);
+  };
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const [isOpenDelete, setIsOpenDelete] = useState(false);
+  const openModalDelete = () => {
+    setIsOpenDelete(true);
+  };
+  const closeModalDelete = () => {
+    setIsOpenDelete(false);
+  };
+
+  const water =
+    item.waterValue >= 999
+      ? Math.round((item.waterValue / 1000) * 100) / 100 + " L"
+      : item.waterValue + " ml";
+
+  return (
+    <div className={css.card}>
+      <svg className={css.bottleIcon}>
+        <use href={sprite + "#icon-water-glass"}></use>
+      </svg>
+      <div className={css.textBox}>
+        <p className={css.ml}>{water}</p>
+        <p className={css.time}>{item.localTime}</p>
+      </div>
+      <div className={css.btnBox}>
+        <button className={css.button} type="button" onClick={openModal}>
+          <svg className={css.btnIcon}>
+            <use href={sprite + "#icon-edit"}></use>
+          </svg>
+        </button>
+        <button className={css.button} type="button" onClick={openModalDelete}>
+          <svg className={css.btnIcon}>
+            <use href={sprite + "#icon-trash"}></use>
+          </svg>
+        </button>
+      </div>
+
+      <div>
+        <BaseModal isOpen={isOpen} onClose={closeModal}>
+          <WaterModal mode={"edit"} onClose={closeModal} water={item} />
+        </BaseModal>
+      </div>
+
+      <GlobalModal
+        isOpen={isOpenDelete}
+        title={"Delete"}
+        onRequestClose={closeModalDelete}
+      >
+        <DeleteModal onRequestClose={closeModalDelete} water={item} />
+      </GlobalModal>
+    </div>
+  );
+}
--- src/components/WaterList/WaterList.jsx
+++ src/components/WaterList/WaterList.jsx
@@ -1,3 +1,42 @@
-export default function WaterList() {
-  return <div>WaterList</div>;
+import { useWater } from "../../hooks/useWater.jsx";
+import { WaterItem } from "../WaterItem/WaterItem.jsx";
+import css from "./WaterList.module.css";
+import { useRef, useEffect } from "react";
+
+export function WaterList() {
+  const { waterRecord } = useWater().waterPerDay;
+
+  function useHorizontalScroll() {
+    const elRef = useRef();
+    useEffect(() => {
+      const el = elRef.current;
+      if (el) {
+        const onWheel = (e) => {
+          if (e.deltaY == 0) return;
+          e.preventDefault();
+          el.scrollTo({
+            left: el.scrollLeft + e.deltaY,
+            behavior: "smooth",
+          });
+        };
+        el.addEventListener("wheel", onWheel);
+        return () => el.removeEventListener("wheel", onWheel);
+      }
+    }, []);
+    return elRef;
+  }
+
+  return (
+    <div ref={useHorizontalScroll()} className={css.waterList}>
+      {waterRecord.length > 0 ? (
+        waterRecord.map((value) => {
+          return <WaterItem key={value._id} item={value} />;
+        })
+      ) : (
+        <div className={css.empty}>
+          <p>🌵It is as dry as a desert</p>
+        </div>
+      )}
+    </div>
+  );
 }
