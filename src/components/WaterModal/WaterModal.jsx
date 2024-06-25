6521f43bdf390273b9ff87dcc387e8dee31ea0f6
MolchanovaOlga <designer.molchanova@gmail.com>
2024-06-25 12:49:40 +0300

    Add props to WaterForm and WaterModal

--- src/components/WaterModal/WaterModal.jsx
+++ src/components/WaterModal/WaterModal.jsx
@@ -3,13 +3,23 @@ import ModalWrapper from "../ModalWrapper/ModalWrapper";
 
 import css from "./WaterModal.module.css";
 
-const WaterModal = ({ isOpen, onCloseModal, title, text }) => {
+const onSubmitDataExam = (data, counter) =>
+  console.log({ ...data, amount: counter });
+
+const WaterModal = ({
+  isOpen,
+  onCloseModal,
+  title = "Add water",
+  text = "Choose a value:",
+  onSubmitData = onSubmitDataExam,
+  initialState,
+}) => {
   return (
     <>
       <ModalWrapper modalIsOpen={isOpen} onCloseModal={onCloseModal}>
         <h2 className={css.mainTitle}>{title}</h2>
         <h3 className={css.title}>{text}</h3>
-        <WaterForm />
+        <WaterForm onSubmitData={onSubmitData} initialState={initialState} />
       </ModalWrapper>
     </>
   );
