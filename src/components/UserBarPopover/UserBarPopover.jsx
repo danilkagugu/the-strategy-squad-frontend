41d68f0085a8f3ae5f47af5e6680dca0c854eb5c
Олег Слюсар <eternityonly1@gmail.com>
2024-06-26 16:32:54 +0300

    add functionality
    work partly

--- src/components/UserBarPopover/UserBarPopover.jsx
+++ src/components/UserBarPopover/UserBarPopover.jsx
@@ -1,44 +1,50 @@
-
-import { useState } from "react";
-import sprite from "../../assets/icons.svg";
-import css from "./UserBarPopover.module.css";
-// import ModalWrapper from '../ModalWrapper/ModalWrapper';
-import UserSettingsModal from "../UserSettingsModal/UserSettingsModal";
-import LogOutModal from "../LogOutModal/LogOutModal"
+import { useState } from 'react';
+import sprite from '../../assets/icons.svg';
+import css from './UserBarPopover.module.css';
+import UserSettingsModal from '../UserSettingsModal/UserSettingsModal';
+import LogOutModal from '../LogOutModal/LogOutModal';
+import scrollController from '../../services/noScroll';
 
 const UserBarPopover = ({ modalRef, modalIsOpen, closeModal, openModal }) => {
+	const [isOpen, setIsOpen] = useState(false);
 
-  const [isOpen, setIsOpen] = useState(false);
+	const onOpenModal = () => {
+		setIsOpen(true);
+		scrollController.disabledScroll();
+	};
+	const onCloseModal = () => {
+		setIsOpen(false);
+		scrollController.enabledScroll();
+	};
 
-  const onOpenModal = () => setIsOpen(true);
-  const onCloseModal = () => setIsOpen(false);
-
-  return (
-    <>
-      {isOpen && (
-        <UserSettingsModal
-          onCloseModal={onCloseModal}
-          isOpen={isOpen}
-          modalRef={modalRef}
-        />
-      )}
-      <div className={css.barPopover}>
-        <button className={css.btnBar} onClick={onOpenModal}>
-          <svg className={css.icon}>
-            <use href={`${sprite}#icon-settings`}></use>
-          </svg>
-          <p className={css.textBarPopover}>Setting</p>
-        </button>
-        <button className={css.btnBar} onClick={openModal}>
-          <svg className={css.icon}>
-            <use href={`${sprite}#icon-log-out`}></use>
-          </svg>
-          <p className={css.textBarPopover}>Log out</p>
-        </button>
-        {modalIsOpen && <LogOutModal modalIsOpen={modalIsOpen} closeModal={closeModal} />}
-      </div>
-    </>
-  );
+	return (
+		<>
+			{isOpen && (
+				<UserSettingsModal
+					onCloseModal={onCloseModal}
+					isOpen={isOpen}
+					modalRef={modalRef}
+				/>
+			)}
+			<div className={css.barPopover}>
+				<button className={css.btnBar} onClick={onOpenModal}>
+					<svg className={css.icon}>
+						<use href={`${sprite}#icon-settings`}></use>
+					</svg>
+					<p className={css.textBarPopover}>Setting</p>
+				</button>
+				<button className={css.btnBar} onClick={openModal}>
+					<svg className={css.icon}>
+						<use href={`${sprite}#icon-log-out`}></use>
+					</svg>
+					<p className={css.textBarPopover}>Log out</p>
+				</button>
+				{modalIsOpen && (
+					<LogOutModal modalIsOpen={modalIsOpen} closeModal={closeModal} />
+				)}
+			</div>
+		</>
+	);
 };
 
 export default UserBarPopover;
--- src/components/UserSettingsModal/UserSettingsModal.jsx
+++ src/components/UserSettingsModal/UserSettingsModal.jsx
@@ -1,24 +1,24 @@
-import ModalWrapper from "../ModalWrapper/ModalWrapper";
-import UserSettingsForm from "../UserSettingsForm/UserSettingsForm";
-import css from "./UserSettingsModal.module.css";
+import ModalWrapper from '../ModalWrapper/ModalWrapper';
+import UserSettingsForm from '../UserSettingsForm/UserSettingsForm';
+import css from './UserSettingsModal.module.css';
 
 const UserSettingsModal = ({ isOpen, onCloseModal, modalRef }) => {
-  return (
-    <>
-      <ModalWrapper
-        modalIsOpen={isOpen}
-        onCloseModal={onCloseModal}
-        contentLabel="User Settings"
-        top="10%"
-        transform="translate(-50%, 0)"
-      >
-        <div ref={modalRef}>
-          <h3 className={css.title}>Setting</h3>
-          <UserSettingsForm />
-        </div>
-      </ModalWrapper>
-    </>
-  );
+	return (
+		<>
+			<ModalWrapper
+				modalIsOpen={isOpen}
+				onCloseModal={onCloseModal}
+				contentLabel='User Settings'
+				top='5%'
+				transform='translate(-50%, 0)'
+			>
+				<div ref={modalRef} className={css.modalContent}>
+					<h3 className={css.title}>Setting</h3>
+					<UserSettingsForm onClose={onCloseModal} />
+				</div>
+			</ModalWrapper>
+		</>
+	);
 };
 
 export default UserSettingsModal;
