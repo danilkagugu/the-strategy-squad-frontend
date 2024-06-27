5e14ca6f9e686d75bc416338839a203cc6ea9856
Danulo <danilyanishevski2001@gmail.com>
2024-06-28 01:29:25 +0300

    upgrade grafic

--- src/helpers/getAmountForDayAndMonth.js
+++ src/helpers/getAmountForDayAndMonth.js
@@ -7,3 +7,15 @@ export const getMonthFromDateStr = (dateStr) => {
   const date = new Date(dateStr.substring(0, 10));
   return date.getMonth() + 1;
 };
+
+export const getCurrentWeek = (date) => {
+  const mondey = new Date(date);
+  mondey.setDate(date.getDate() - date.getDay() + 1);
+  const weekDates = [];
+  for (let i = 0; i < 7; i++) {
+    const day = new Date(mondey);
+    day.setDate(mondey.getDate() + i);
+    weekDates.push(day);
+  }
+  return weekDates;
+};
