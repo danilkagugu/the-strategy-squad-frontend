4ccafc93173a74d5b584e49c0f84dbe63db4bd7c
volodymyr1v <volodumur.verhyn@gmail.com>
2024-06-27 20:56:18 +0300

    corect twice ChooseDate

--- src/components/ChooseDate/ChooseDate.jsx
+++ src/components/ChooseDate/ChooseDate.jsx
@@ -1,23 +1,21 @@
 import style from "./ChooseDate.module.css";
 export default function ChooseDate({ data }) {
-  const date = new Date(data);
+  const [day, month] = data.split(", ");
+  const currentYear = new Date().getFullYear();
+  const date = new Date(`${month} ${day}, ${currentYear}`);
 
   const currentDate = new Date();
+
   const isToday =
     date.getDate() === currentDate.getDate() &&
     date.getMonth() === currentDate.getMonth() &&
-    date.getFullYear() === currentDate.getFullYear();
-
-  let monthName = date.toLocaleString("en-us", { month: "long" });
-  let dayInMonth = date.getDate();
+    date.getFullYear() === currentYear;
 
   let formData;
   if (isToday) {
     formData = "Today";
-  } else if (isNaN(dayInMonth) || isNaN(date.getTime())) {
-    formData = "Invalid Date";
   } else {
-    formData = `${dayInMonth}, ${monthName}`;
+    formData = `${day}, ${month}`;
   }
   return <p className={style.text}>{formData}</p>;
 }
