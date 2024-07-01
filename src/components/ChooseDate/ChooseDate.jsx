db7174bfb80a6870eed7686b3b5216cd077c7ee4
Danulo <danilyanishevski2001@gmail.com>
2024-07-01 13:02:19 +0300

    test

--- src/components/ChooseDate/ChooseDate.jsx
+++ src/components/ChooseDate/ChooseDate.jsx
@@ -1,17 +1,36 @@
 import { useTranslation } from "react-i18next";
 import style from "./ChooseDate.module.css";
+
 export default function ChooseDate({ data }) {
   const { t } = useTranslation();
   const date = new Date(data);
   let monthName = date.toLocaleString("en-us", { month: "long" });
   let dayInMonth = date.getDate();
 
+  // Використання перекладу місяців
+  const translatedMonthNames = {
+    January: t("january"),
+    February: t("february"),
+    March: t("march"),
+    April: t("april"),
+    May: t("may"),
+    June: t("june"),
+    July: t("july"),
+    August: t("august"),
+    September: t("september"),
+    October: t("october"),
+    November: t("november"),
+    December: t("december"),
+  };
+
+  const translatedMonthName = translatedMonthNames[monthName];
+
   if (isNaN(dayInMonth) || isNaN(date.getTime())) {
     dayInMonth = `${t("today")}`;
     monthName = "";
   } else {
     dayInMonth = `${dayInMonth}`;
-    monthName = `, ${monthName}`;
+    monthName = `, ${translatedMonthName}`;
   }
   const formData = `${dayInMonth}${monthName}`;
 
