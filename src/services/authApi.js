972596e8e0f08f16442a90891d7d64cc010d3050
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2024-07-01 18:29:14 +0100

    chenged api url to render.com

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -1,52 +1,49 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:3030";
+export const BASE_URL = "https://the-strategy-squad-backend.onrender.com";
 
 export const instance = axios.create({
-    baseURL: "http://localhost:3030",
+  baseURL: BASE_URL,
 });
 
 export const setToken = (token) => {
-    instance.defaults.headers.common.Authorization = `Bearer ${token}`;
-
+  instance.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
 
 export const clearToken = () => {
-    instance.defaults.headers.common.Authorization = "";
+  instance.defaults.headers.common.Authorization = "";
 };
 
 export const requestSignUp = async (formData) => {
+  const { data } = await instance.post("/api/users/register", formData);
 
-    const { data } = await instance.post("/api/users/register", formData);
-
-    return data;
-
+  return data;
 };
 export const requestSignIn = async (formData) => {
-    const { data } = await instance.post("/api/users/login", formData);
-    setToken(data.token);
-    return data;
+  const { data } = await instance.post("/api/users/login", formData);
+  setToken(data.token);
+  return data;
 };
 
 export const requestGetCurrentUser = async () => {
-    const { data } = await instance.get("/api/users/current");
+  const { data } = await instance.get("/api/users/current");
 
-    return data;
+  return data;
 };
 
 export const requestLogOut = async () => {
-    const { data } = await instance.post("/api/users/logout");
+  const { data } = await instance.post("/api/users/logout");
 
-    return data;
+  return data;
 };
 
 export const requestUpdate = async (body) => {
-    const data = await instance.patch("/api/users/update", body);
+  const data = await instance.patch("/api/users/update", body);
 
-    return data;
+  return data;
 };
 
 export const requestGoogleSignUp = async () => {
-    const { data } = await instance.get("/api/auth/google");
-    setToken(data.token);
+  const { data } = await instance.get("/api/auth/google");
+  setToken(data.token);
 };
